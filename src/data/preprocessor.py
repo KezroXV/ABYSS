e677b47c75b8172dd42b2a6bea9390ac3a908582
KezroXV <kezro10@gmail.com>
2026-02-03 09:31:52 +0100

    Refactor GithubScraper to optimize commit retrieval process and enhance error handling for improved reliability.

---
+++ src/data/preprocessor.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import numpy as np
+import os
+import datetime
+
+class DataPreprocessor:
+
+    def __init__(self):
+        self.data = None
+
+    def load_data(self, file_path):
+        df = pd.read_csv(file_path)
+        num_rows = len(df)
+        print(f"Commit count loaded : {num_rows}")
+        return df
+
+    def clean_data(self, df):
+        df.drop_duplicates(subset=['sha'], keep='first', inplace=True)
+        df['author'] = df['author'].fillna('Unknown')
+        df['message'] = df['message'].fillna('No Message')
+        print(f"Commit count after cleaning : {len(df)}")
+        return df
+
+    def extract_temporal_features(self, df):
+        df['date'] = pd.to_datetime(df['date'], errors='coerce')
+        df = df[df['date'].notna()]
+        df['hour'] = df['date'].dt.hour
+        df['day_of_week'] = df['date'].dt.dayofweek
+        df['is_weekend'] = (df['day_of_week'] >= 5).astype(int)
+        df['is_night'] = ((df['hour'] >= 22) | (df['hour'] < 6)).astype(int)
+        print(f"temp features extracted")
+        return df
+    
+    def add_commit_metrics(self, df):
+        df['total_lines_changed'] =np.random.randint(1,500, size=len(df))
+        df['files_changed'] = np.random.randint(1,20, size=len(df))
+        df['is_large_commit'] = (df['total_lines_changed'] > 200).astype(int)
+        print(f"commit metrics added")
+        return df
+    
+    def save_clean_data(self,df, filepath):
+        os.makedirs("data/processed", exist_ok=True)
+        df.to_csv(filepath, index=False)
+        print(f"Clean data saved to {filepath}")
+        return df
+
+    def main(self):
+        df = self.load_data("data/processed/commits_labeled.csv")
+        df = self.clean_data(df)
+        df = self.extract_temporal_features(df)
+        df = self.add_commit_metrics(df)
+        self.save_clean_data(df, "data/processed/commits_clean.csv")
+        print(df.head())
+        print(df.info())
+        return df
+
+if __name__ == "__main__":
+    dp = DataPreprocessor()
+    dp.main()
